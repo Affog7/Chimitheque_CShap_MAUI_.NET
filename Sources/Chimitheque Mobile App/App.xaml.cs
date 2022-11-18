b998f727737ab2b66517e4794c9ba2d59bc5242e
Victor Pérez Ngounou <vngounou26@gmail.com>
2022-11-18 09:56:47 +0100

    Update Auth View

--- Sources/Chimitheque Mobile App/App.xaml.cs
+++ Sources/Chimitheque Mobile App/App.xaml.cs
@@ -1,5 +1,6 @@
 using ChimithequeLib.APisManagers;
 using ChimithequeLib;
+using Chimitheque_Mobile_App.View;
 
 namespace Chimitheque_Mobile_App;
 
@@ -7,14 +8,14 @@ public partial class App : Application
 {
     public App()
     {
-        AuthService authService = new AuthService();
-        var token = authService.GetTokenAsync("admin@chimitheque.fr", "chimitheque");
-        var httpClient = authService.httpClient;
+        //AuthService authService = new AuthService();
+        //var token = authService.GetTokenAsync("admin@chimitheque.fr", "chimitheque");
+        //var httpClient = authService.httpClient;
 
-        var ddd = new ProductsManager().GetProductsById(1245,httpClient);
-        //InitializeComponent();
+        //var ddd = new ProductsManager().GetProductsById(1245,httpClient);
+            InitializeComponent();
 
             MainPage = new AuthenticationView();
-        }
+        
     }
 }
